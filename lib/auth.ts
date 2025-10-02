7b3383089f0dca3de2afa24ca6fb574d5734f37a
Jake Walklate <jakewalklate@protonmail.com>
2025-10-03 03:04:34 +1000

    omg secrets

--- lib/auth.ts
+++ lib/auth.ts
@@ -9,6 +9,7 @@ const prisma = new PrismaClient();
 
 export const authOptions = {
   adapter: PrismaAdapter(prisma),
+  secret: process.env.NEXTAUTH_SECRET,
   providers: [
     FacebookProvider({
       clientId: process.env.FACEBOOK_ID!,
