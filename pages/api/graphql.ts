f65c287bbb06a3034dcd2c75b45d66b5ce2828f1
Jake Walklate <jakewalklate@protonmail.com>
2025-10-03 02:44:41 +1000

    test vercel

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -1,6 +1,11 @@
-import { createYoga } from 'graphql-yoga'
-import { schema } from './schema'
- 
-const yoga = createYoga({ schema, graphiql: true, graphqlEndpoint: '/api/graphql' });
+import { createYoga } from 'graphql-yoga';
+import { schema } from '@/graphql/schema';
+import { prisma } from '@/lib/prisma';
+
+const yoga = createYoga({
+  schema,
+  graphiql: true, // playground
+  context: ({ request }) => ({ prisma, request }),
+});
 
 export default yoga;
