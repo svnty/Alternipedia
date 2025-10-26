1caa59d655090bcc3565d895b4bd0334668cbfab
Jake Walklate <jakewalklate@protonmail.com>
2025-10-26 23:41:33 +1100

    fuck you chatGPT

--- app/robots.ts
+++ app/robots.ts
@@ -10,7 +10,6 @@ export default function robots(): MetadataRoute.Robots {
       disallow: [
         '/*/wiki/*/wikipedia',
         "/api/",
-        "/_next/",
       ],
       allow: ['/'],
     },
